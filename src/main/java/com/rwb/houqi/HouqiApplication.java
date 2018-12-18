2435fcd934d1886059d78a4ca37ac7e9f8b39888
ran <1269713675@qq.com>
2018-12-18 15:19:34 +0800

    "this is project init"

---
+++ src/main/java/com/rwb/houqi/HouqiApplication.java
@@ -0,0 +1,17 @@
+package com.rwb.houqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@MapperScan("com.rwb.houqi.mapper")
+@SpringBootApplication
+public class HouqiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HouqiApplication.class, args);
+    }
+
+}
+
